4f5db39ab50d4ba017a491fbee46cb656819ee81
Tadeo <td12233a@gmail.com>
2021-04-20 09:11:43 -0500

    small fix

--- src/main/java/com/catsinred/cartercraft/CarterCraft.java
+++ src/main/java/com/catsinred/cartercraft/CarterCraft.java
@@ -7,12 +7,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CarterCraft extends JavaPlugin {
     private Logger LOGGER = getLogger();
     private Server SERVER = getServer();
-    
+
     @Override
     public void onEnable() {
         LOGGER.info("Starting CarterCraft...");
 
-        if (SERVER.getPluginManager().isPluginEnabled("Citizens")) {
+        if (!SERVER.getPluginManager().isPluginEnabled("Citizens")) {
             LOGGER.severe("Could not find Citizens2. Disabling.");
             SERVER.getPluginManager().disablePlugin(this);
             return;
